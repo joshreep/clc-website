1145c77010a55d6478ff2bad12729154a672e213
Josh Reep <joshreep@gmail.com>
2019-10-19 16:08:50 -0400

    markup on topnav

--- ui/components/TopNav.tsx
+++ ui/components/TopNav.tsx
@@ -1,1 +1,20 @@
 import { memo } from 'react'
+import Link from 'next/link'
+import Navbar from 'react-bootstrap/Navbar'
+import Image from 'react-bootstrap/Image'
+
+interface Props {}
+
+const TopNav = (props: Props) => {
+    return (
+        <Navbar bg="light">
+            <Link href="/" passHref>
+                <Navbar.Brand>
+                    <Image src="/static/logo-full.png" height="30" alt="Covenant Life Church Logo" className="d-inline-block align-top" />
+                </Navbar.Brand>
+            </Link>
+        </Navbar>
+    )
+}
+
+export default memo(TopNav)
