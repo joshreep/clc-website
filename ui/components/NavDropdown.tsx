b1090160fd3737fbf916a534de1f841fca7093d2
Josh Reep <josh.reep@sstid.com>
2020-02-22 14:27:53 -0500

    Adds custom dropdown component with hover capability

---
+++ ui/components/NavDropdown.tsx
@@ -0,0 +1,34 @@
+import React, { memo, useState, ReactNode } from 'react'
+
+import BSNavDropdown from 'react-bootstrap/NavDropdown'
+
+import useDebounce from 'hooks/useDebounce'
+
+interface Props {
+    href?: string
+    id: string
+    title: string
+    children: ReactNode
+}
+
+const NavDropdown = ({ children, id, title }: Props) => {
+    const [show, setShow] = useState(false)
+    const debouncedShow = useDebounce(show, 250)
+
+    return (
+        <BSNavDropdown
+            id={id}
+            title={title}
+            show={show || debouncedShow}
+            onMouseEnter={() => setShow(true)}
+            onMouseLeave={() => setShow(false)}
+        >
+            {children}
+        </BSNavDropdown>
+    )
+}
+
+NavDropdown.Item = BSNavDropdown.Item
+NavDropdown.Divider = BSNavDropdown.Divider
+
+export default NavDropdown
