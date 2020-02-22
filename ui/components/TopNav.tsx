3cca31346c71e32f186a36bc981fb2cc6c127c06
Josh Reep <josh.reep@sstid.com>
2020-02-22 14:28:11 -0500

    finishes topnav component

--- ui/components/TopNav.tsx
+++ ui/components/TopNav.tsx
@@ -1,18 +1,64 @@
 import { memo } from 'react'
 import Link from 'next/link'
-import Navbar from 'react-bootstrap/Navbar'
+
+import Container from 'react-bootstrap/Container'
 import Image from 'react-bootstrap/Image'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 
-interface Props {}
+import NavDropdown from './NavDropdown'
 
-const TopNav = (props: Props) => {
+const TopNav = () => {
     return (
         <Navbar bg="light">
-            <Link href="/" passHref>
-                <Navbar.Brand>
-                    <Image src="/static/logo-full.png" height="30" alt="Covenant Life Church Logo" className="d-inline-block align-top" />
-                </Navbar.Brand>
-            </Link>
+            <Container>
+                <Link href="/" passHref>
+                    <Navbar.Brand>
+                        <Image
+                            src="/logo-full.png"
+                            height="50"
+                            alt="Covenant Life Church Logo"
+                            className="d-inline-block align-top"
+                        />
+                    </Navbar.Brand>
+                </Link>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <NavDropdown id="nav-about-us" title="About Us" href="/about">
+                        <Link href="/about" passHref>
+                            <NavDropdown.Item>About Us</NavDropdown.Item>
+                        </Link>
+                        <NavDropdown.Divider />
+                        <Link href="/about/sundays" passHref>
+                            <NavDropdown.Item>Sunday Gatherings</NavDropdown.Item>
+                        </Link>
+                        <Link href="/about/what-we-believe" passHref>
+                            <NavDropdown.Item>Statement Of Faith</NavDropdown.Item>
+                        </Link>
+                        <Link href="/about/home-group" passHref>
+                            <NavDropdown.Item>Small Groups</NavDropdown.Item>
+                        </Link>
+                        <Link href="/about/new-here" passHref>
+                            <NavDropdown.Item>New Here?</NavDropdown.Item>
+                        </Link>
+                    </NavDropdown>
+                    <Link href="/messages" passHref>
+                        <Nav.Link>Messages</Nav.Link>
+                    </Link>
+                    <Link href="https://tithe.ly/give_new/www/#/tithely/give-one-time/26957" passHref>
+                        <Nav.Link>Give</Nav.Link>
+                    </Link>
+                    <Link href="https://covenantlifeonline.elvanto.net/login" passHref>
+                        <Nav.Link>Members</Nav.Link>
+                    </Link>
+                    <Link href="/live" passHref>
+                        <Nav.Link>Live</Nav.Link>
+                    </Link>
+                    <Link href="/camp" passHref>
+                        <Nav.Link>Kids Camp</Nav.Link>
+                    </Link>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     )
 }
