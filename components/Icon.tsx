c73515884d8461bc1943196083b7267d2a248bd0
Josh Reep <josh.reep@sstid.com>
2020-03-10 07:26:51 -0400

    adds icon component

---
+++ components/Icon.tsx
@@ -0,0 +1,10 @@
+import { memo } from 'react'
+import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
+
+interface Props extends FontAwesomeIconProps {}
+
+const Icon = (props: Props) => {
+    return <FontAwesomeIcon {...props} />
+}
+
+export default memo(Icon)
