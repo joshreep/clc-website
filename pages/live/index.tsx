459b8d24e5dceea5c17ff02303ef354cdce8a4f3
Josh Reep <josh.reep@sstid.com>
2020-03-16 22:11:57 -0400

    Adds initial content for live stream page

--- pages/live/index.tsx
+++ pages/live/index.tsx
@@ -1,10 +1,26 @@
 import React, { memo } from 'react'
 import { NextPage } from 'next'
+import Container from 'react-bootstrap/Container'
 
 interface Props {}
 
 const LiveStreamPage: NextPage<Props> = props => {
-    return <div>Live Stream Page{JSON.stringify(props)}</div>
+    return (
+        <Container className="my-5" as="article">
+            <h1>
+                <span className="bordered">Live Stream</span>
+            </h1>
+            <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
+                <iframe
+                    src="https://vimeo.com/event/15393/embed"
+                    frameBorder="0"
+                    allow="autoplay; fullscreen"
+                    allowFullScreen
+                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                />
+            </div>
+        </Container>
+    )
 }
 
 export default memo(LiveStreamPage)
