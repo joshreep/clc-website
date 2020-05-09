888fab185c51b17b7c7b5aa78210609e25863b63
Josh Reep <josh.reep@sstid.com>
2020-05-08 20:34:41 -0400

    fixes lint errors and formats

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
-const path = require('path');
-const withSass = require('@zeit/next-sass');
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path')
+const withSass = require('@zeit/next-sass')
 
 module.exports = withSass({
-    webpack(config, options) {
-        config.resolve.alias['api'] = path.join(__dirname, 'api');
-        config.resolve.alias['components'] = path.join(__dirname, 'components');
-        config.resolve.alias['data'] = path.join(__dirname, 'data');
-        config.resolve.alias['styles'] = path.join(__dirname, 'styles');
-        config.resolve.alias['types'] = path.join(__dirname, 'types');
-        config.resolve.alias['utilities'] = path.join(__dirname, 'utilities');
-        config.resolve.alias['hooks'] = path.join(__dirname, 'hooks');
-        config.resolve.alias['src'] = path.join(__dirname);
+    webpack(config) {
+        config.resolve.alias['api'] = path.join(__dirname, 'api')
+        config.resolve.alias['components'] = path.join(__dirname, 'components')
+        config.resolve.alias['data'] = path.join(__dirname, 'data')
+        config.resolve.alias['errors'] = path.join(__dirname, 'errors')
+        config.resolve.alias['styles'] = path.join(__dirname, 'styles')
+        config.resolve.alias['types'] = path.join(__dirname, 'types')
+        config.resolve.alias['utilities'] = path.join(__dirname, 'utilities')
+        config.resolve.alias['hooks'] = path.join(__dirname, 'hooks')
+        config.resolve.alias['lib'] = path.join(__dirname, 'lib')
+        config.resolve.alias['src'] = path.join(__dirname)
 
-        return config;
+        return config
     }
-});
+})
